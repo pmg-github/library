ee176489375eb13bbba6155a83476e752efc9a22
Rien Pieters <rpi@pmgroup.be>
2025-06-12 15:53:32 +0200

    Fixed gitignore

---
+++ dist/library.js
@@ -0,0 +1,33 @@
+import { defineComponent as o, createElementBlock as i, openBlock as l, normalizeClass as s, renderSlot as a, createTextVNode as r, toDisplayString as d } from "vue";
+const u = ["disabled"], m = /* @__PURE__ */ o({
+  __name: "PMGButton",
+  props: {
+    text: { type: String, required: !1 },
+    disabled: { type: Boolean, default: !1 },
+    size: {
+      type: String,
+      default: "md"
+    },
+    theme: {
+      type: String,
+      default: "default"
+    }
+  },
+  emits: ["click"],
+  setup(e) {
+    return o({
+      name: "JetButton"
+    }), (t, n) => (l(), i("button", {
+      onClick: n[0] || (n[0] = (f) => t.$emit("click")),
+      disabled: e.disabled,
+      class: s(["jet-btn font-black bg-red-500 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition duration-150 ease-in-out", `jet-btn-${e.theme}`])
+    }, [
+      a(t.$slots, "default", {}, () => [
+        r(d(e.text), 1)
+      ])
+    ], 10, u));
+  }
+});
+export {
+  m as PMGButton
+};
