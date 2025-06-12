5ea066d6b0f9db451908a35d2c6c0b075e9366a8
Rien Pieters <rpi@pmgroup.be>
2025-06-12 16:22:12 +0200

    renamed lib

--- dist/pmg-library.js
+++ dist/pmg-library.js
@@ -1,5 +1,5 @@
-import { defineComponent as o, createElementBlock as i, openBlock as l, normalizeClass as s, renderSlot as a, createTextVNode as r, toDisplayString as d } from "vue";
-const u = ["disabled"], m = /* @__PURE__ */ o({
+import { defineComponent as o, createElementBlock as i, openBlock as l, normalizeClass as s, renderSlot as a, createTextVNode as d, toDisplayString as r } from "vue";
+const u = ["disabled"], f = /* @__PURE__ */ o({
   __name: "PMGButton",
   props: {
     text: { type: String, required: !1 },
@@ -18,16 +18,16 @@ const u = ["disabled"], m = /* @__PURE__ */ o({
     return o({
       name: "JetButton"
     }), (t, n) => (l(), i("button", {
-      onClick: n[0] || (n[0] = (f) => t.$emit("click")),
+      onClick: n[0] || (n[0] = (c) => t.$emit("click")),
       disabled: e.disabled,
       class: s(["jet-btn font-black bg-red-500 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition duration-150 ease-in-out", `jet-btn-${e.theme}`])
     }, [
       a(t.$slots, "default", {}, () => [
-        r(d(e.text), 1)
+        d(r(e.text), 1)
       ])
     ], 10, u));
   }
-});
+}), b = { PMGButton: f };
 export {
-  m as PMGButton
+  b as default
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import PMGButton from "./components/button/PMGButton.vue";
 
-export { PMGButton };
+export default { PMGButton };
