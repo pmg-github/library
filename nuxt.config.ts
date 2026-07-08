d5f1d9bc8297156f70f2fc515d4648712fb45929
RienPieters <pietersrien7@gmail.com>
2026-07-08 16:34:50 +0200

    test auto import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   srcDir: "src",
   css: ["./src/index.css"],
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
   build: {
     transpile: ["vee-validate", "@tiptap/vue-3"],
   },
